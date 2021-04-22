0c74426d8580a45a18101ca1cdd73db4326c1961
Alexandre Annic <alexandre.annic1@gmail.com>
2021-04-22 22:40:02 +0200

    Stick to native Set API

--- src/useSetState/UseSetState.ts
+++ src/useSetState/UseSetState.ts
@@ -1,19 +1,42 @@
 import {useState} from 'react'
 
-type ReturnType<T> = [Set<T>, (t: T) => void, (t: T) => void];
+interface ReturnType<T> {
+  add: (t: T | T[]) => void
+  delete: (t: T) => boolean
+  clear: () => void
+  values: () => Iterable<T>
+  size: number
+  has: (t: T) => boolean
+}
 
 export const useSetState = <T>(initialValue: T[] = []): ReturnType<T> => {
   const [set, setSet] = useState(new Set<T>(initialValue))
 
-  const add = (t: T): void => {
-    set.add(t)
-    setSet(new Set(set))
+  const add = (t: T | T[]): void => {
+    if (Array.isArray(t)) {
+      const newSet = new Set(t)
+      setSet(newSet)
+    } else {
+      set.add(t)
+      const newSet = new Set(set)
+      setSet(newSet)
+    }
   }
 
-  const remove = (t: T): void => {
-    set.delete(t)
+  const remove = (t: T): boolean => {
+    const returnValue = set.delete(t)
     setSet(new Set(set))
+    return returnValue
   }
 
-  return [set, add, remove]
+  const clear = () => setSet(new Set())
+
+  return {
+    has: (t: T) => set.has(t),
+    size: set.size,
+    values: () => set.values(),
+    add,
+    delete: remove,
+    clear,
+  }
 }
