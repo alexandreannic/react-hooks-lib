86a06e274a1be4a2b32dc62bfee791b812b6400f
Alexandre Annic <alexandre.annic1@gmail.com>
2021-12-17 11:41:11 +0100

    UseMemoFn: Fix typing issue with non nullable params

--- src/useMemoFn/UseMemoFn.ts
+++ src/useMemoFn/UseMemoFn.ts
@@ -1,6 +1,7 @@
 import {useMemo} from 'react'
 
-export const useMemoFn = <T, R>(dep: T | undefined, map: (_: T) => R) => {
+export const useMemoFn = <T, R>(dep: T, map: (_: T) => R): undefined extends T ? (R | undefined) : R => {
+  // @ts-ignore
   return useMemo(() => {
     return dep ? map(dep) : undefined
   }, [dep])
