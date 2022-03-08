42736f54b499e934ed75b5b49e22afdd63e0ed1e
Alexandre Annic <alexandre.annic1@gmail.com>
2022-03-08 18:46:21 +0100

    UseEffectFn: Fix boolean usage

--- src/useEffectFn/UseEffectFn.ts
+++ src/useEffectFn/UseEffectFn.ts
@@ -2,6 +2,6 @@ import {useEffect} from 'react'
 
 export const useEffectFn = <T, R>(dep: T | undefined, map: (_: T) => void) => {
   return useEffect(() => {
-    if (dep) map(dep)
+    if (dep !== undefined) map(dep)
   }, [dep])
 }
