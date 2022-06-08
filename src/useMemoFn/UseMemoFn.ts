8d6b90a0152f2628fe660b16dc406198737caf8a
Alexandre Annic <alexandre.annic1@gmail.com>
2022-06-08 14:03:54 +0200

    UseMemoFn: fix typing issue

--- src/useMemoFn/UseMemoFn.ts
+++ src/useMemoFn/UseMemoFn.ts
@@ -1,6 +1,6 @@
 import {useMemo} from 'react'
 
-export const useMemoFn = <T, R>(dep: T | undefined, map: (_: T) => R): undefined extends T ? undefined : R => {
+export const useMemoFn = <T, R>(dep: T | undefined, map: (_: T) => R): R | undefined => {
   // @ts-ignore
   return useMemo(() => {
     return dep ? map(dep) : undefined
