a2b25b9fbdc48c288308d4c3ad00fa5fb4ae5445
Alexandre Annic <alexandre.annic1@gmail.com>
2022-06-13 16:02:41 +0200

    Paginate: Fix sideeffect in setState

--- src/usePaginate/UsePaginate.ts
+++ src/usePaginate/UsePaginate.ts
@@ -1,4 +1,4 @@
-import {Fetch, useFetcher} from '..'
+import {Fetch, UseFetcher, useFetcher} from '..'
 import {Dispatch, SetStateAction, useCallback, useState} from 'react'
 
 export interface Paginate<T> {
@@ -15,21 +15,18 @@ export interface ISearch<T = any> {
   sortBy?: keyof T
 }
 
-export interface UsePaginate<T, S, E = any> {
+export interface UsePaginate<T, S, E = any> extends Pick<UseFetcher<any, E>, 'error' | 'setEntity' | 'clearCache'> {
   list?: Paginate<T>,
-  error?: E
-  fetching: boolean,
   fetch: Fetch<(...args: any[]) => Promise<Paginate<T>>>
   filters: S,
   updateFilters: (_: SetStateAction<S>, params?: UpdateFiltersParams) => void,
-  setEntity: Dispatch<SetStateAction<Paginate<T> | undefined>>,
   clearFilters: () => void,
   pageNumber: number
   initialFilters: S
+  fetching: boolean,
 }
 
 export interface UpdateFiltersParams {
-  noRefetch?: boolean
   preserveOffset?: boolean
 }
 
@@ -43,15 +40,12 @@ export const usePaginate = <T, S extends ISearch, E = any>(
   const [filters, setFilters] = useState<S>({...defaultFilters, ...initialFilters})
   const {entity: list, error, loading: fetching, fetch, setEntity, clearCache} = useFetcher<typeof fetcher, E>(fetcher, undefined, mapError)
 
-  const updateFilters = useCallback((update: SetStateAction<S>, {noRefetch, preserveOffset}: UpdateFiltersParams = {}) => {
+  const updateFilters = useCallback((update: SetStateAction<S>, {preserveOffset}: UpdateFiltersParams = {}) => {
     setFilters(mutableFilters => {
       const previous = {...mutableFilters}
       const updatedFilters = typeof update === 'function' ? update(mutableFilters) : update
       if (!preserveOffset && previous.offset === updatedFilters.offset && previous.limit === updatedFilters.limit) {
         updatedFilters.offset = 0
-      }
-      if (!noRefetch) {
-        fetch({force: true, clean: false}, updatedFilters)
       }
       return updatedFilters
     })
@@ -70,5 +64,6 @@ export const usePaginate = <T, S extends ISearch, E = any>(
     clearFilters,
     initialFilters,
     setEntity,
+    clearCache,
   }
 }
