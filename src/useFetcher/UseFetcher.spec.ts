abd38711d859e77626065ce7bb223991142f9eff
Alexandre Annic <alexandre.annic1@gmail.com>
2023-03-06 14:01:00 +0200

    Try to test
    Clean

--- src/useFetcher/UseFetcher.spec.ts
+++ src/useFetcher/UseFetcher.spec.ts
@@ -1,12 +1,14 @@
 import {useFetcher} from './UseFetcher'
 import {createUserApi} from '../useCrudList/UseCrudList.spec'
+import {renderHook} from '@testing-library/react-hooks'
 
 describe('UseFetcher', function () {
 
   it('. Typing playground', async function () {
+
     const userApi = createUserApi()
 
-    const fetcher = useFetcher(userApi.fetch)
+    const fetcher = renderHook(() => useFetcher(userApi.fetch)).result.current
     fetcher.fetch({force: true, clean: true}, {onlineOnly: true}).then(console.log)
   })
 })
